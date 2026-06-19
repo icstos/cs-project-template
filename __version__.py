02a38d2a728f5286ae370246efd188f2c66233af
cs <cs@cstos.com>
2026-06-19 22:00:55 +0800

    init

---
+++ __version__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.1"
+__author__ = "Shawn Chen"
+__author_email__ = "cs@cstos.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2026 Shawn Chen"
+__url__ = "https://github.com/icstos/cs-project-template"
+
+
+__description__ = ""
